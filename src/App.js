c6d26aeee541f9b024199986e98da573978c83d9
Munish Verma <munish.verma12@gmail.com>
2024-02-19 11:07:50 +0530

    Use state example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Incerement</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
