062f3e98b997fea884e3e4db5e9bbe28d18a5aca
Munish Verma <munish.verma12@gmail.com>
2024-02-19 20:49:27 +0530

    Updates

--- src/pages/hooks-explained/useMemo/utils.js
+++
@@ -1,7 +0,0 @@
-export const initialItems = new Array(29_999_999).fill(null).map((_, i) => (
-    {
-    id: i, // Ensure each item has a unique ID
-    isSelected: i === 29_999_998 // This will only select the last item initially; adjust as needed
-  }
-  ));
-  
